0b7de5889535c96f482c7df7ec232db0b0e5691c
Romain Beauxis <toots@rastageeks.org>
2017-12-27 11:08:08 -0600

    Add ao depext after PR was accepted upstream.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -22,7 +22,7 @@ if [ -n "${BUILD_BASE}" ]; then
   fi
 fi
 
-SKIPPED="ao-windows.0.2.1 ocaml-windows32.4.04.0 conf-gcc-windows64.1 lwt-zmq-windows.2.0.1 zmq-windows.4.0-7"
+SKIPPED="ocaml-windows32.4.04.0 conf-gcc-windows64.1 lwt-zmq-windows.2.0.1 zmq-windows.4.0-7"
 
 printf "Building pretest image.."
 DOCKER_CMD="docker build -f ${TEST_PWD}/Dockerfile.windows-x64-pretest -t ocamlcross/windows-x64-pretest:latest --build-arg \"OPAM_SKIPPED=${SKIPPED}\" ${BASE_PWD}"
